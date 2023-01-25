1e2a261a809dde40333c6d204538c91776a27e68
Victor <victorchike247@gmail.com>
2023-01-25 19:39:24 +0000

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'mangolia': '#f0f6ff',
+      'alabaster': '#fafbff',
+      'blue': {
+        'marine': '#02295a',
+        'purplish': '#473dff',
+        'pastel': '#adbeff',
+        'light': '#bfe2fd'
+      },
+      'red': {
+        'strawberry': '#ed3548'
+      },
+      'gray': {
+        'cool': '#9699ab',
+        'light': '#d6d9e6'
+      }
+    },
     extend: {},
   },
   plugins: [],
