22977c95080899df2b42e629f387808084b1edff
Victor <victorchike247@gmail.com>
2023-01-26 21:53:09 +0000

    added sidelink

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarLinkComponent } from '../sidebar-link/sidebar-link.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarLinkComponent],
   templateUrl: './sidebar.component.html',
   styles: []
 })
 export class SidebarComponent {
-
+  links = LINKS
+  activeStep = 1
 }
+
+interface Link {
+  step: number
+  stage: string
+}
+
+const LINKS: Link[] = [
+  { step: 1, stage: 'your info'},
+  { step: 2, stage: 'select plan'},
+  { step: 3, stage: 'add-ons'},
+  { step: 4, stage: 'summary'},
+]
