e436f5861d7fd58a1c2a4b4e5037714c3aa6946a
Victor <victorchike247@gmail.com>
2023-01-30 20:07:26 +0000

    connected billing to plans

--- src/app/plan-step/plan-step.component.ts
+++ src/app/plan-step/plan-step.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlanRadioComponent } from './plan-radio.component';
 import { PlanToggleComponent } from './plan-toggle.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-plan-step',
@@ -11,12 +12,8 @@ import { PlanToggleComponent } from './plan-toggle.component';
   styles: []
 })
 export class PlanStepComponent {
-  monthPlans = [...BASEPLANS]
-  yearPlans = BASEPLANS.map(plan => {
-    const yearPlan = { ...plan}
-    yearPlan.amount *= 10
-    return yearPlan
-  })
+
+  plans = new BehaviorSubject(MonthPlans)
 
   selectedPlan = 'arcade'
   annualPlan = false
@@ -27,6 +24,9 @@ export class PlanStepComponent {
 
   changeBilling() {
     this.annualPlan = !this.annualPlan
+
+    const displayPlans = this.annualPlan ? YearPlans : MonthPlans
+    this.plans.next(displayPlans)
   }
 }
 
@@ -41,3 +41,9 @@ const BASEPLANS: Plan[] = [
   { name: 'advanced', amount: 12, imgUrl: 'assets/images/icon-advanced.svg' },
   { name: 'pro', amount: 15, imgUrl: 'assets/images/icon-pro.svg' }
 ]
+const MonthPlans = [...BASEPLANS]
+const YearPlans = BASEPLANS.map(plan => {
+    const yearPlan = { ...plan}
+    yearPlan.amount *= 10
+    return yearPlan
+  })
