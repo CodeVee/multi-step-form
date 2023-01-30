f07976b8935bce65444799b13e42777d6a0d4a42
Victor <victorchike247@gmail.com>
2023-01-30 20:13:33 +0000

    added annual discount

--- src/app/plan-step/plan-radio.component.ts
+++ src/app/plan-step/plan-radio.component.ts
@@ -9,11 +9,12 @@ import { CommonModule } from '@angular/common';
     <button
     (click)="selected.emit(planName)"
     [ngClass]="{'border-blue-purplish bg-alabaster': active, 'border-gray-light hover:border-blue-purplish bg-white': !active}"
-    class="h-64 w-full border rounded-bs pl-1.6 pb-1.6 pt-8 flex flex-col justify-between">
+    class="w-full border rounded-bs pl-1.6 pb-1.6 pt-8 flex flex-col gap-16">
       <img [src]="planImg" [alt]="planName">
       <div class="flex flex-col gap-0.8 text-left">
         <h4 class="capitalize font-medium text-bs leading-7 text-blue-marine">{{ planName }}</h4>
         <span class="font-normal text-sb leading-6 text-gray-cool">$ {{ planAmount }}/mo</span>
+        <span *ngIf="annual" class="text-blue-marine font-normal text-xl">2 months free</span>
       </div>
     </button>
   `,
@@ -22,6 +23,7 @@ import { CommonModule } from '@angular/common';
 export class PlanRadioComponent {
 
   @Input() active: boolean | undefined
+  @Input() annual: boolean | undefined
   @Input() planName: string | undefined
   @Input() planImg: string | undefined
   @Input() planAmount: number | undefined
