c5e3aa3734538ada34dddf79e2cec06fedf93ded
Victor <victorchike247@gmail.com>
2023-01-31 20:32:40 +0000

    styled checkbox

--- src/app/addon-step/addon-checkbox.component.ts
+++ src/app/addon-step/addon-checkbox.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
       [ngClass]="{'border-blue-purplish bg-alabaster': checked, 'border-gray-light hover:border-blue-purplish bg-white': !checked}"
       (click)="selected.emit()"
       class="flex items-center gap-2.4 px-2.4 h-32 border rounded-bs w-full">
-      <input type="checkbox" [checked]="checked" class="checked:text-red-strawberry bg-blue-purplish">
+      <input type="checkbox" [checked]="checked" class=" rounded-lg checked:text-blue-purplish focus:ring-offset-0 focus:ring-transparent border border-gray-light cursor-pointer bg-white h-8 w-8">
       <div class="flex flex-col flex-1 gap-0.8 text-left">
         <h4 class="capitalize font-medium text-bs leading-7 text-blue-marine">{{ header }}</h4>
         <h5 class="font-normal text-sb leading-6 text-gray-cool">{{ sub }}</h5>
